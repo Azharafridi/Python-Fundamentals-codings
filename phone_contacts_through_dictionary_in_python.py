c66394ce8967002402b3f84c17ad6b8015aaac44
Azharafridi <99399706+Azharafridi@users.noreply.github.com>
2023-08-21 14:24:02 +0500

    Add files via upload

---
+++ phone_contacts_through_dictionary_in_python.py
@@ -0,0 +1,28 @@
+# this coding is for making, deleting, veiwing and updating the contacts of a phone
+
+# global variable for choicing
+
+view = 1
+addnew = 2
+update = 3
+delete = 4
+quit = 5
+
+def main():
+
+
+def veiw_contact():
+
+
+def add_new_contact():
+
+
+def update_contact():
+
+def delete_contact():
+
+
+
+
+
+
